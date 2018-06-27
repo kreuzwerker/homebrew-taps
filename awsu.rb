2baf172718c9b14486b06b4a21456e907294c208
Joern Barthel <joern.barthel@kreuzwerker.de>
2018-06-27 10:15:57 +0200

    Upgraded awsu.

--- awsu.rb
+++ awsu.rb
@@ -3,10 +3,10 @@ class Awsu < Formula
   desc "Amazon Web Services Switch User (awsu)"
   homepage "https://github.com/kreuzwerker/awsu"
 
-  version "2.2.0"
-  url "https://github.com/kreuzwerker/awsu/releases/download/2.2.0/awsu-darwin-amd64",
+  version "2.2.1"
+  url "https://github.com/kreuzwerker/awsu/releases/download/2.2.1/awsu-darwin-amd64",
     :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "1338b67b45cce0516e9b4360d191ed12ccfcdbc23584ec831200b63162b403d2"
+  sha256 "03f3855bf3a5da204f3d0e443bc6010884287e006779588bd3fec25692808637"
 
   bottle :unneeded
 
