f778578a3e2d770a467a38e6c44258c093daba46
Jonas Petersen <jox@mindfloaters.com>
2018-06-28 15:15:33 +0200

    Update awsu.rb
    Removed token requirement.

--- awsu.rb
+++ awsu.rb
@@ -4,8 +4,7 @@ class Awsu < Formula
   homepage "https://github.com/kreuzwerker/awsu"
 
   version "2.2.1"
-  url "https://github.com/kreuzwerker/awsu/releases/download/2.2.1/awsu-darwin-amd64",
-    :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  url "https://github.com/kreuzwerker/awsu/releases/download/2.2.1/awsu-darwin-amd64"
   sha256 "03f3855bf3a5da204f3d0e443bc6010884287e006779588bd3fec25692808637"
 
   bottle :unneeded
