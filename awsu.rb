699f40091b28f319810b0225fbb07d03ec531efb
Joern Barthel <joern.barthel@kreuzwerker.de>
2018-09-28 11:06:21 +0200

    Added awsu 2.3.0RC1.

--- awsu.rb
+++ awsu.rb
@@ -3,9 +3,9 @@ class Awsu < Formula
   desc "Amazon Web Services Switch User (awsu)"
   homepage "https://github.com/kreuzwerker/awsu"
 
-  version "2.2.1"
-  url "https://github.com/kreuzwerker/awsu/releases/download/2.2.1/awsu-darwin-amd64"
-  sha256 "03f3855bf3a5da204f3d0e443bc6010884287e006779588bd3fec25692808637"
+  version "2.3.0RC1"
+  url "https://github.com/kreuzwerker/awsu/releases/download/2.3.0RC1/awsu-darwin-amd64"
+  sha256 "685a2843e4116ef721eee940f9df989c88ff4918cad384c591c9c801765e7507"
 
   bottle :unneeded
 
